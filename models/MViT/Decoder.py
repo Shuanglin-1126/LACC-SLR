dc1871d87b85f19a805654f0ddbe043abcdd50fa
Che Xiao <shuanglinche@gmail.com>
2025-12-30 16:38:23 +0800

    Update copyright information in Decoder.py
    Added copyright information for 2025 modifications.

--- models/MViT/Decoder.py
+++ models/MViT/Decoder.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2023 Facebook, Inc. and its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Modifications Copyright 2025 Che Xiao
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+
 from functools import partial
 import numpy as np
 import torch
